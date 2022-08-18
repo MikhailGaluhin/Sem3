19651258f761528b75ce4f0fd0df8b0fa4c3830e
NensAleksa <108027067+NensAleksa@users.noreply.github.com>
2022-08-19 00:28:30 +0300

    решили 23 задачу

---
+++ Task23/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string? CubeFind(int N)
+{
+string result = "";
+for(int i=1; i<N; i++)
+{
+result += $"{Math.Pow(i,3)}";
+}
+return result;
+}
+
+string result = CubeFind(N);
+Console.WriteLine(result);
