859149b6ad2da332be535e43b91b9dfaa428e88c
NensAleksa <108027067+NensAleksa@users.noreply.github.com>
2022-08-18 23:58:21 +0300

    решили 19 задачу

---
+++ Task19/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите  число: ");
+string? number = Console.ReadLine();
+
+bool CheckingPolydrome(string number)
+{
+    if (number[0] == number[4] || number[1] == number[3])
+    {
+        return true;
+    }
+    else if (number!.Length == 5)
+    {
+        throw new Exception("Число не походит под день недели");
+    }
+    else return false;
+
+}
+bool result = CheckingPolydrome(number);
+
+if(result == true)
+{
+Console.WriteLine("Число является полиндромом");
+}
+else
+{
+Console.WriteLine("Число не является полиндромом");
+}
